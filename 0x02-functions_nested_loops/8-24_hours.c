779281c8da75e3e8eb40dd5f0d37e9de629bc152
root <aloemo77@gmail.com>
2022-09-14 03:59:06 -0700

    task 8 commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,10 +7,10 @@
 
 void jack_bauer(void)
 {
-	int hours = 0
-		int minutes = 0
+	int hours = 0;
+		int minutes = 0;
 		int hours_remainder;
-	int mins_remainder
+	int mins_remainder;
 
 		while (hours <= 23)
 		{
