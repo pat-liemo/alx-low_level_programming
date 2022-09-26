5b1f1c5a232a5cadde548406b6e339cabd15eb47
root <aloemo77@gmail.com>
2022-09-26 10:41:42 -0700

    task 0-7 commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy- copies memory area
+ * @dest: memory area to be copied to
+ * @src: memory area to be copied from
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to be copied memory block
+ */
+
+char *_memcpy(char *dest, cha *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
