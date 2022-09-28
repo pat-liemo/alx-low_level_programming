84e7ce7634711f6baf5aa9909a678c4123db3b6c
root <aloemo77@gmail.com>
2022-09-28 11:54:16 -0700

    task commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,32 +3,36 @@
 /**
  * is_prime_number- returns the 1 if n is prime
  * @n: number to be checked
+ * @c: iterator
  *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+
+/**
+ * is_prime_number- detects if an input is prime
+ * @n: input number
  * Return: 1 if n is prime, 0 otherwise
  */
 
 int is_prime_number(int n)
 {
-	int start = n / 2;
-
-	if (n <= 1)
+	if (n == 0)
 		return (0);
-	return (is_prime(n, start));
-}
-
-/**
- * is_prime- returns the 1 if n is prime
- * @n: number to be checked
- * @start: number to start checking from
- *
- * Return: 1 if n is prime, 0 otherwise
- */
-
-int is_prime(int n, int start)
-{
-	if (start <= 1)
-		return (1);
-	else if (n % start == 0)
+	else if (n < 0)
 		return (0);
-	return (is_prime(n, start - 1));
+	else if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
 }
